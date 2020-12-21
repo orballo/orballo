4e59113bfeda70e9c21c9cccbb1bf660926c3ead
Eduardo Campaña <asrielo@gmail.com>
2020-12-21 04:02:09 +0100

    wip

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-const logoDark = document.getElementById("logo");
-const logoLight = document.getElementById("logo-light");
-const body = document.querySelector("body");
-const main = document.querySelector("main");
+window.addEventListener("load", () => {
+  const logoDark = document.getElementById("logo");
+  const logoLight = document.getElementById("logo-light");
+  const body = document.querySelector("body");
+  const main = document.querySelector("main");
 
-let clicked = false;
+  let clicked = false;
 
-logoDark.addEventListener("click", () => {
-  logoDark.style.display = "none";
-  logoLight.style.display = "block";
-  body.classList.add("inverted");
-  main.classList.add("inverted");
+  logoDark.addEventListener("click", () => {
+    logoDark.style.display = "none";
+    logoLight.style.display = "block";
+    body.classList.add("inverted");
+    main.classList.add("inverted");
 
-  if (window.plausible && !clicked) {
-    window.plausible("Click on Logo");
-    clicked = true;
-  }
-});
+    if (window.plausible && !clicked) {
+      window.plausible("Click on Logo");
+      clicked = true;
+    }
+  });
 
-logoLight.addEventListener("click", () => {
-  logoDark.style.display = "block";
-  logoLight.style.display = "none";
-  body.classList.remove("inverted");
-  main.classList.remove("inverted");
+  logoLight.addEventListener("click", () => {
+    logoDark.style.display = "block";
+    logoLight.style.display = "none";
+    body.classList.remove("inverted");
+    main.classList.remove("inverted");
+  });
 });

9c927cacdcd508d008eee02f590f22c0703a6d8d
Eduardo Campaña <asrielo@gmail.com>
2020-12-21 03:51:52 +0100

    wip

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ const logoLight = document.getElementById("logo-light");
 const body = document.querySelector("body");
 const main = document.querySelector("main");
 
+let clicked = false;
+
 logoDark.addEventListener("click", () => {
   logoDark.style.display = "none";
   logoLight.style.display = "block";
   body.classList.add("inverted");
   main.classList.add("inverted");
+
+  if (window.plausible && !clicked) {
+    window.plausible("Click on Logo");
+    clicked = true;
+  }
 });
 
 logoLight.addEventListener("click", () => {
